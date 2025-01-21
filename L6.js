47b2ceefdf7cc5131a539a6ff92ec7597badb1c8
Maher Ali <mahiralimattummal@gmail.com>
2025-01-21 23:59:49 +0530

    Create L6.js

---
+++ L6.js
@@ -0,0 +1,11 @@
+//question: 217. Contains Duplicate
+
+var containsDuplicate = function (nums) {
+  let set = new Set();
+  for (let i = 0; i < nums.length; i++) {
+    if (set.has(nums[i])) return true;
+    set.add(nums[i]);
+  }
+  return false;
+};
+console.log(containsDuplicate([1, 2, 3, 1]));
