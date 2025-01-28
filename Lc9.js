13e8896c0e61cb8b944241a4aaf144fc50d9e049
Maher Ali <mahiralimattummal@gmail.com>
2025-01-28 23:54:56 +0530

    Create Lc9.js

---
+++ Lc9.js
@@ -0,0 +1,14 @@
+//Remove Duplicates from Sorted Array
+
+function removeDuplicates(nums) {
+    let index = 0;
+
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] !== nums[index]) {
+            index++;
+            nums[index] = nums[i];
+        }
+    }
+
+    return index + 1;
+}
