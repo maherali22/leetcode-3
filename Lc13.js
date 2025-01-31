5911a177e8e55ce82750a48083ad26e69b6d7576
Maher Ali <mahiralimattummal@gmail.com>
2025-01-31 23:58:17 +0530

    Create Lc13.js

---
+++ Lc13.js
@@ -0,0 +1,10 @@
+var isPalindrome = function(x) {
+   let a=x.toString().split('').reverse().join('');
+   let b=x.toString();
+   if(b===a){
+    return true
+    }else{return false} 
+};
+
+let c=isPalindrome(-121);
+console.log(c);
